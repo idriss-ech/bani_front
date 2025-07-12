a4a44508a253846c586ad36ea0e19cc88ec44cf7
idriss <inscrag@gmail.com>
2025-07-12 13:32:24 +0100

    config genuine

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com", "localhost"],
+    domains: ["images.unsplash.com", "localhost", "genuine-thrill-338e6da1f5.media.strapiapp.com"],
     // Ou utilise remotePatterns pour plus de sécurité :
     // remotePatterns: [
     //   {
